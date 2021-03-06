ea679c2032e5a33b4be5fa2a3458bf19da225e9c
JamesT94 <jamest9401@gmail.com>
2021-03-06 02:40:27 +0000

    Added stuff to game-sim-1

--- game-sim-1.py
+++ game-sim-1.py
@@ -1,5 +1,13 @@
 """
-Info?
+PLAN
+
+Create a several stations and have an agent automatically work at/between them
+- Mine
+- Refinery
+- Store
+
+Have the agent mine ore, refine it and sell it for gold. Then repeat.
+
 """
 
 import pygame
@@ -24,14 +32,15 @@ class Mine(object):
 
     def station_mine_ore(self, player):
 
-        if self.progress > 0.99:
+        # Triggers when the player reaches 100% progress
+        if self.progress > 99:
             player.inventory['copper_ore'] += 1
             self.progress = 0
             print("Successfully mined some copper ore")
             print("Player now has {} copper ore".format(player.inventory['copper_ore']))
         else:
             self.progress += self.mine_rate
-            time.sleep(0.2)
+            time.sleep(0.5)
             print('Mining Progress: {}'.format(self.progress))
 
     def draw(self, display):
@@ -47,16 +56,16 @@ class Refinery(object):
         self.refine_rate = refine_rate
         self.progress = 0
 
-    def station_refine_ore(self, ore, player):
+    def station_refine_ore(self, player):
 
-        if self.progress > 0.99:
+        if self.progress > 99:
             player.inventory['copper_bar'] += 1
             self.progress = 0
             print("Successfully refined a copper bar")
             print("Player now has {} copper bar(s)".format(player.inventory['copper_bar']))
         else:
             self.progress += self.refine_rate
-            time.sleep(0.2)
+            time.sleep(0.5)
             print('Refining Progress: {}'.format(self.progress))
 
     def draw(self, display):
@@ -96,6 +105,36 @@ class Player(object):
     def draw(self, display):
         pygame.draw.circle(display, WHITE, (self.x, self.y), self.radius)
 
+    def define_action(self):
+        if self.inventory['copper_ore'] > 3:
+            self.action = 'Refine Ore'
+        if self.inventory['copper_bar'] > 3 and self.inventory == 0:
+            self.action = 'Sell Goods'
+        if self.inventory['copper_ore'] == 0 and self.inventory['copper_bar'] == 0:
+            self.action = 'Mine Ore'
+
+    def take_action(self, station_list):
+        if self.action == 'Mine Ore':
+            mine = self.locate_mine(station_list)
+            self.player_mine_ore(mine)
+
+        if self.action == 'Refine Ore':
+            refinery = self.locate_refinery(station_list)
+            self.player_refine_ore(refinery)
+
+        if self.action == 'Sell Goods':
+            store = self.locate_store(station_list)
+            self.player_mine_ore(store)
+
+    def locate_mine(self, station_list):
+        return station_list['mines'][0]
+
+    def locate_refinery(self, station_list):
+        return station_list['refineries'][0]
+
+    def locate_store(self, station_list):
+        return station_list['stores'][0]
+
     def player_mine_ore(self, mine):
         # move to the mine, if close enough start to mine
         close_enough = True
@@ -104,13 +143,13 @@ class Player(object):
 
     def player_refine_ore(self, refinery):
         # move to the mine, if close enough start to mine
-        close_enough = False
+        close_enough = True
         if close_enough:
             refinery.station_refine_ore(self)
 
     def player_sell_goods(self, store):
         # move to the mine, if close enough start to mine
-        close_enough = False
+        close_enough = True
         if close_enough:
             store.station_sell_goods(self)
 
@@ -119,37 +158,37 @@ def draw_game(display, station_list, player_list):
 
     display.fill(BLACK)
 
-    for station in station_list:
-        station.draw(display)
+    for stations in station_list:
+        for station in stations:
+            station.draw(display)
 
     for player in player_list:
         player.draw(display)
 
-    pygame.display.update()
-
 
 # Initialise game window
+pygame.init()
 pygame.display.set_caption('Mine, Refine, Sell')
-frame_size_x = 200
-frame_size_y = 200
-game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
+frame_size_x = 400
+frame_size_y = 400
+game_window = pygame.display.set_mode([frame_size_x, frame_size_y])
 
 fps = pygame.time.Clock()
 render_gfx = True
 
 # --- Main --- #
 my_player = Player(10, 10, 1)
-copper_mine = Mine(150, 10, 0.01, 'Copper Ore')
-copper_refinery = Refinery(120, 100, 0.02)
+copper_mine = Mine(150, 10, mine_rate=5, ore='Copper Ore')
+copper_refinery = Refinery(120, 100, refine_rate=5)
 copper_store = Store(20, 120)
 
 players = [my_player]
-stations = [copper_mine, copper_refinery, copper_store]
+stations = {'mines': [copper_mine],
+            'refineries': [copper_refinery],
+            'stores': [copper_store]}
 
-# Game Loop Starts Here
+# Game loop starts here
 while render_gfx:
-    fps.tick(2)
-    draw_game(game_window, stations, players)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -157,15 +196,21 @@ while render_gfx:
             pygame.quit()
             quit()
 
-    while my_player.action == 'Mine Ore':
-        my_player.player_mine_ore(copper_mine)
+    """
+    player defines his action
+    player takes his action
+    game loop continues and repeats
+    """
 
-    my_player.action = 'Refine Ore'
-    while my_player.action == 'Refine Ore':
-        my_player.player_refine_ore(copper_refinery)
+    my_player.define_action()
+    my_player.take_action(stations)
 
-    my_player.action = 'Sell Goods'
-    while my_player.action == 'Sell Goods':
-        my_player.player_sell_goods(copper_store)
+    if my_player.gold > 10:
+        print('Player final gold value: {}'.format(my_player.gold))
+        pygame.quit()
 
-    print('Player final gold value: {}'.format(my_player.gold))
+    draw_game(game_window, stations.values(), players)
+    pygame.display.flip()
+    fps.tick(30)  # FPS
+
+pygame.quit()
